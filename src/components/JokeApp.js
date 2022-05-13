27f605166293663f1bb8e62ea12aad9b7d9fe1c1
KenasiaTurner <kenasi123@gmail.com>
2022-05-13 14:12:44 -0400

    adds the joke app

---
+++ src/components/JokeApp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class JokeApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            joke: '',
+            isLoading: false
+        };
+    }
+
+    
+    _fetchJoke = () => {
+        this.setState({ 
+            isLoading:true,
+        }, () => {
+            const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+            fetch(url)
+                .then(response => response.json())
+                .then(jokeJson => {
+                    this.setState({
+                        joke: jokeJson.value,
+                        isLoading: false
+                    }, () => {
+                        console.log('New joke stored');
+                    });
+                })
+        })
+    }
+}
+
+export default JokeApp;
